12abcb1400eb3b4787381e0634ff65740d6eca9b
Guilherme Ribeiro <guilherme.rrl@gmail.com>
2022-04-10 17:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,67 @@
+﻿using CadastroClientes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro(int id)
+        {
+            Cliente cliente = new Cliente();
+
+            if (id != default (int)){
+                cliente = Cliente.SelecionarId(id);
+            }
+            
+            return View(cliente);
+        }
+
+
+        public ActionResult SelecionarTodos()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            lista = Cliente.Selecionar();
+            var result = new
+            {
+                data = lista
+            };
+            return new JsonResult(result); 
+        }
+
+        public int InserirAtualizarCliente(Cliente cliente)
+        {
+                  
+            if(cliente.Id == default(int))
+            {
+                int res = Cliente.InserirCliente(cliente);
+                return res;
+            }
+
+            else
+            {
+                Cliente.AtualizarCliente(cliente);
+                return cliente.Id;
+            }
+
+            return 0;
+        }
+
+        public ActionResult SelecionarPorID(int id)
+        {
+            Cliente cliente = new Cliente();
+            cliente = Cliente.SelecionarId(id);
+            return Json(cliente);
+        }
+
+        public void ExcluirCliente(int id)
+        {
+            Cliente.ExcluirCliente(id);
+        }
+    }
+}
---
+++ CadastroClientes/Controllers/EnderecoController.cs
@@ -0,0 +1,51 @@
+﻿using CadastroClientes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CadastroClientes.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult SelecionarTodos()
+        {
+            List<Endereco> lista = new List<Endereco>();
+            lista = Endereco.Selecionar();
+            var result = new
+            {
+                data = lista
+        };
+            return new JsonResult(lista);
+        }
+        
+        public void InserirAtualizarEndereco(Endereco endereco)
+        {
+                    
+            if (endereco.Id != default(int))
+            {
+                Endereco.InserirEndereco(endereco);
+            }
+
+            else
+            {
+                Endereco.AtualizarEndereco(endereco);
+            }
+        }
+
+        public ActionResult SelecionarPorID(int id)
+        {
+            Endereco endereco = new Endereco();
+            endereco = Endereco.SelecionarId(id);
+            return Json(endereco);
+        }
+
+        public void ExcluirEndereco(int id)
+        {
+            Endereco.ExcluirEndereco(id);
+        }
+    }
+}
